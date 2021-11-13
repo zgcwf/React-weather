95f542a6aa74d580ea61899089f4d470eb42cc5d
zgc <1935083614@qq.com>
2021-11-13 23:12:13 +0800

    源码+笔记

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -1,12 +1,12 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Layout, Menu } from 'antd';
 import './index.css'
-import {withRouter} from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import {
   UserOutlined
 } from '@ant-design/icons';
 import axios from 'axios'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 const { Sider } = Layout;
 const { SubMenu } = Menu
 
@@ -48,62 +48,64 @@ const { SubMenu } = Menu
 //   }
 // ]
 const iconList = {
-  "/home":<UserOutlined />,
-  "/user-manage":<UserOutlined />,
-  "/user-manage/list":<UserOutlined />,
-  "/right-manage":<UserOutlined />,
-  "/right-manage/role/list":<UserOutlined />,
-  "/right-manage/right/list":<UserOutlined />
+  "/home": <UserOutlined />,
+  "/user-manage": <UserOutlined />,
+  "/user-manage/list": <UserOutlined />,
+  "/right-manage": <UserOutlined />,
+  "/right-manage/role/list": <UserOutlined />,
+  "/right-manage/right/list": <UserOutlined />
   //.......
 }
 
 
 function SideMenu(props) {
   const [meun, setMeun] = useState([])
-  useEffect(()=>{
-    axios.get("/rights?_embed=children").then(res=>{
+  useEffect(() => {
+    axios.get("/rights?_embed=children").then(res => {
       console.log(res.data)
       setMeun(res.data)
     })
-  },[])
+  }, [])
 
-  const {role:{rights}} = JSON.parse(localStorage.getItem("token"))
+  const { role: { rights } } = JSON.parse(localStorage.getItem("token"))
 
-  const checkPagePermission = (item)=>{
+  const checkPagePermission = (item) => {
     return item.pagepermisson && rights.includes(item.key)
   }
-  const renderMenu = (menuList)=>{
-    return menuList.map(item=>{
-      if(item.children?.length>0 && checkPagePermission(item)){
+  const renderMenu = (menuList) => {
+    return menuList.map(item => {
+      if (item.children?.length > 0 && checkPagePermission(item)) {
         return <SubMenu key={item.key} icon={iconList[item.key]} title={item.title}>
-           { renderMenu(item.children) }
+          {renderMenu(item.children)}
         </SubMenu>
+      } else {
+        return checkPagePermission(item) && <Menu.Item key={item.key} icon={iconList[item.key]} onClick={() => {
+          //  console.log(props)
+          props.history.push(item.key)
+        }}>{item.title}</Menu.Item>
       }
 
-      return checkPagePermission(item) && <Menu.Item key={item.key} icon={iconList[item.key]}  onClick={()=>{
-        //  console.log(props)
-        props.history.push(item.key)
-      }}>{item.title}</Menu.Item>
+
     })
   }
 
   // console.log(props.location.pathname)
   const selectKeys = [props.location.pathname]
-  const openKeys = ["/"+props.location.pathname.split("/")[1]]
+  const openKeys = ["/" + props.location.pathname.split("/")[1]]
   return (
     <Sider trigger={null} collapsible collapsed={props.isCollapsed} >
-      <div style={{display:"flex",height:"100%","flexDirection":"column"}}>
+      <div style={{ display: "flex", height: "100%", "flexDirection": "column" }}>
         <div className="logo" >全球新闻发布管理系统</div>
-        <div style={{flex:1,"overflow":"auto"}}>
+        <div style={{ flex: 1, "overflow": "auto" }}>
           <Menu theme="dark" mode="inline" selectedKeys={selectKeys} className="aaaaaaa" defaultOpenKeys={openKeys}>
-              {renderMenu(meun)}
+            {renderMenu(meun)}
           </Menu>
         </div>
       </div>
     </Sider>
   )
 }
-const mapStateToProps = ({CollApsedReducer:{isCollapsed}})=>({
+const mapStateToProps = ({ CollApsedReducer: { isCollapsed } }) => ({
   isCollapsed
 })
 export default connect(mapStateToProps)(withRouter(SideMenu))
--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -8,14 +8,16 @@ export default function IndexRouter() {
     return (
         <HashRouter>
             <Switch>
+                {/* 路由组件，component={组件}展示 */}
                 <Route path="/login" component={Login} />
                 <Route path="/news" component={News} />
                 <Route path="/detail/:id" component={Detail} />
                 {/* <Route path="/" component={NewsSandBox}/> */}
-                {/* 如果已授权，则展示 NewsSandBox 组件，否则重定向到 login*/}
+                {/* 可以写成一个函数，函数的返回值就是你要渲染的组件，如果已授权，则展示 NewsSandBox 组件，否则重定向到 login*/}
                 <Route path="/" render={() =>
                     localStorage.getItem("token") ?
                         <NewsSandBox ></NewsSandBox> :
+                        // 普通组件，标签展示
                         <Redirect to="/login" />
                 } />
             </Switch>
