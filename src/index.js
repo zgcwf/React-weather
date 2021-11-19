eb827f33226f816e86eb11aa4684f816ac391a6e
zgc <1935083614@qq.com>
2021-11-19 22:53:47 +0800

    源码+笔记

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ import App from './App';
 import './util/http'
 
 ReactDOM.render(
-    <App />
+  <App />
   ,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 // reportWebVitals();
